e1323e2cba2ed83b6f60ea52168c78cf7b6629da
florianvandersteen <florianvandersteen@gmail.com>
2018-12-16 23:57:47 +0100

    improves functionality and design

--- dictionary_converter.py
+++ dictionary_converter.py
@@ -1,29 +1,33 @@
 import sys
 
 def dictionarymaker(length, dictfile):
-	with open("nl_words.txt", "r") as nl_dict:
-		sys.stdout=open(dictfile,"w")
-		for word in nl_dict:
-			# adds the correct words with "ij" in them
-			if len(word) == length + 1 and word[0].islower():
-				for i in range(len(word) - 1):
-					if word[i] == "'" or word[i] == "." or word[i] == "-" or word[i] == " ":
-						break
-					if word[i] == 'i' and word[i + 1] == 'j':
-						print(word, end="")
-			# adds the correct words with the correct length
-			if len(word) == length and word[0].islower():
-				for i in range(len(word) - 1):
-					if word[i] == 'i' and word[i + 1] == 'j':
-						break
-					if word[i] == "'" or word[i] == "." or word[i] == "-" or word[i] == " ":
-						break
-				# if we've reached here we're safe to write the word
-				else:
-					print(word, end="") 
-		sys.stdout.close()
-# add code that deletes jij-bak
+	with open("nl_words.txt", "r") as fileIn, open(dictfile, "w") as file_out:
+		for line in fileIn:
+			if len(line) == length + 1:
+				if character_verification(line) and ij_verification(line):
+					file_out.write(line)
+
+			if len(line) == length:
+				if character_verification(line) and not ij_verification(line):
+					file_out.write(line)
+
+def character_verification(line):
+	banned_chars = ["-", "'", ".", " "]
+	for index in range(len(line)):
+		if line[index] in banned_chars or line[index].isupper():
+			return False
+	else:
+		return True
+
+def ij_verification(line):
+	for index in range(len(line)):
+		if line[index] == "i" and line[index + 1] == "j":
+			return True
+	else:
+		return False
+
 
 if __name__ == "__main__":
 	# length is actually one higher
 	dictionarymaker(7, "nl_words_6.txt")
+	
